be1b8769aeef8d4b77d16e174789e501be2c4ff1
christian <christiangouw168@gmail.com>
2025-11-14 00:46:06 +0700

    refactor: improve minHeight calculation in MobileContainer for better responsiveness

--- components/layout/MobileContainer.tsx
+++ components/layout/MobileContainer.tsx
@@ -18,12 +18,12 @@ export default function MobileContainer({
 }: MobileContainerProps) {
 	return (
 		<Container
-			className={cn("py-3 bg-zinc-50 px-4", noPadding && "p-0", className)}
-			style={{
-				...(withMinHeight && {
-					minHeight: `calc(100vh - 57px)`,
-				}),
-			}}
+			className={cn(
+				"py-3 bg-zinc-50 px-4",
+				noPadding && "p-0",
+				withMinHeight && `min-h-[calc(100vh-${APP_BAR_MAX_HEIGHT}px)]`,
+				className
+			)}
 			{...props}
 		>
 			{children}
