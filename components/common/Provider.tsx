ee3eeea7723c272dee1c6dd2dfd17f455efd32d3
christian <christiangouw168@gmail.com>
2026-03-10 02:15:09 +0700

    refactor: optimize query client initialization in Provider component using useState for improved performance

--- components/common/Provider.tsx
+++ components/common/Provider.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createQueryClient } from "@/utils/createQueryClient";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = createQueryClient();
+  const [queryClient] = useState(() => createQueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
